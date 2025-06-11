05a75cd73a7bf6360d57e6e08e672009c735a00f
whosefriendA <whosefrienda@163.com>
2025-06-11 23:55:20 +0800

    fix: key return too simple,add more info
    Signed-off-by: whosefriendA <whosefrienda@163.com>

--- client/naming.go
+++ client/naming.go
@@ -2,9 +2,9 @@ package client
 
 import (
 	"encoding/json"
+	"fmt"
 	"time"
 
-	"github.com/whosefriendA/firEtcd/kvraft"
 	"github.com/whosefriendA/firEtcd/pkg/firlog"
 )
 
@@ -36,36 +36,47 @@ func (n *Node) Unmarshal(data []byte) {
 }
 
 func (n *Node) Key() string {
-	return n.Name
+	// 使用 fmt.Sprintf 格式化字符串，确保所有部分都被包含
+	return fmt.Sprintf("/%s/%s/%s:%s", n.Env, n.AppId, n.Name, n.Port)
 }
 
-func (n *Node) SetNode(ck *Clerk, TTL time.Duration) error {
+func (n *Node) SetNode(ck KVStore, TTL time.Duration) error {
 	err := ck.Put(n.Key(), n.Marshal(), TTL)
 	if err != nil {
-		firlog.Logger.Fatal(err)
+		return fmt.Errorf("database unavailable: %w", err) // 将错误返回
 	}
-	return err
+	return nil
 }
 
-func (n *Node) SetNode_Watch(ck *Clerk) (cancle func()) {
+func (n *Node) SetNode_Watch(ck WatchDoger) (cancle func()) {
 	return ck.WatchDog(n.Key(), n.Marshal())
 }
 
-func GetNode(ck *Clerk, name string) ([]*Node, error) {
-	datas, err := ck.GetWithPrefix(name)
+// 让 GetNode 依赖 KVStore 接口
+// 注意：为了能构造出正确的 prefix key，函数可能需要更多参数，比如 env
+func GetNode(ck KVStore, appName string, env string) ([]*Node, error) {
+	// 假设 prefix key 的格式是 /env/appName/
+	prefixKey := fmt.Sprintf("/%s/%s/", env, appName)
+	datas, err := ck.GetWithPrefix(prefixKey)
 	if err != nil {
-		if err == kvraft.ErrNil {
-			return nil, nil
-		}
-		firlog.Logger.Fatalln(err)
+		// 如果错误是 ErrNil，说明没找到，这不是一个真正的错误
+		//if errors.Is(err, ErrNil) { // 假设您有一个 ErrNil
+		//	return []*Node{}, nil
+		//}
 		return nil, err
 	}
-	// firLog.Logger.Debugln("raw data:", datas)
-	nodes := make([]*Node, len(datas))
-	for i := range datas {
-		n := &Node{}
-		n.Unmarshal([]byte(datas[i]))
-		nodes[i] = n
+
+	if len(datas) == 0 {
+		return []*Node{}, nil
+	}
+
+	nodes := make([]*Node, 0, len(datas))
+	for _, data := range datas {
+		node := &Node{}
+		// 假设使用 json unmarshal
+		if e := json.Unmarshal(data, node); e == nil {
+			nodes = append(nodes, node)
+		}
 	}
 	return nodes, nil
 }
