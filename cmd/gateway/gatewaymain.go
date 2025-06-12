3a19b6376dc1d9c479481d58755fb145067e153c
whosefriendA <whosefrienda@163.com>
2025-06-12 22:22:14 +0800

    update pb
    Signed-off-by: whosefriendA <whosefrienda@163.com>

--- cmd/gateway/gatewaymain.go
+++ cmd/gateway/gatewaymain.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/whosefriendA/firEtcd/gateway"
 	"github.com/whosefriendA/firEtcd/pkg/firconfig"
+	"github.com/whosefriendA/firEtcd/pkg/firlog"
 )
 
 var gate *gateway.Gateway
@@ -10,6 +11,7 @@ var gate *gateway.Gateway
 func init() {
 	var conf firconfig.Gateway
 	firconfig.Init("config.yml", &conf)
+	firlog.Logger.Debugf("Gateway <UNK> %s %s", conf.Port, conf.BaseUrl)
 	gate = gateway.NewGateway(conf)
 }
 
